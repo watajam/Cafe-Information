c6c3bf14a29dc3c149bd58a42d76796884a28ca4
wataru <wata124coron@icloud.com>
2021-05-19 00:48:46 +0900

    💄 rounded-fullに変更

--- components/atoms/PrimaryButton.jsx
+++ components/atoms/PrimaryButton.jsx
@@ -1,6 +1,6 @@
 export const PrimaryButton = ({ children }) => {
   return (
-    <button class="bg-yellow-500 hover:opacity-80 focus:outline-none w-36 h-12  text-white font-bold rounded-lg  ">
+    <button class="bg-yellow-500 hover:opacity-80 focus:outline-none w-36 h-12  text-white font-bold rounded-full  ">
       {children}
     </button>
   );
