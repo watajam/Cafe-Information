43e89860ae749e93343f6f7149fd693e733fe0b3
wataru <wata124coron@icloud.com>
2021-05-19 00:55:20 +0900

    💄 shadow-2xl、ボタンサイズの変更

--- components/atoms/PrimaryButton.jsx
+++ components/atoms/PrimaryButton.jsx
@@ -1,6 +1,6 @@
 export const PrimaryButton = ({ children }) => {
   return (
-    <button class="bg-yellow-500 hover:opacity-80 focus:outline-none w-36 h-12  text-white font-bold rounded-full  ">
+    <button class="bg-yellow-500 hover:opacity-80 focus:outline-none shadow-2xl w-40 h-12   text-white font-bold rounded-full  ">
       {children}
     </button>
   );
