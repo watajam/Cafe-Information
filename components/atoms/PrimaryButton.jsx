b1127b4b149962b0b69fc05f7d2586cebc54965d
wataru <wata124coron@icloud.com>
2021-05-19 00:41:48 +0900

    💄 PrimaryButtonの変更

--- components/atoms/PrimaryButton.jsx
+++ components/atoms/PrimaryButton.jsx
@@ -1,6 +1,6 @@
 export const PrimaryButton = ({ children }) => {
   return (
-    <button class="bg-yellow-500 hover:bg-yellow-400  w-40 h-12 focus:outline-none text-white font-bold rounded-full ">
+    <button class="bg-yellow-500 hover:opacity-80 focus:outline-none w-36 h-12  text-white font-bold rounded-lg  ">
       {children}
     </button>
   );
