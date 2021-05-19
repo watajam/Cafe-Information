70d7b640113a5f7e426f2c9cf700ef99b36332e0
wataru <wata124coron@icloud.com>
2021-05-19 23:51:20 +0900

    ✨ 検索欄の作成

---
+++ components/molecules/button/SearchInput.jsx
@@ -0,0 +1,9 @@
+import { PrimaryButton } from "../../atoms/button/PrimaryButton"
+
+export const SearchInput = () => {
+  return (
+    <div class="flex  ">
+      <PrimaryButton>検索</PrimaryButton>
+    </div>
+  );
+}
