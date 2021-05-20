d7edf66fbe2777dd6a7cb1fbdcefa773c14e8f00
wataru <wata124coron@icloud.com>
2021-05-21 01:44:43 +0900

    💄 SecondaryButtonの追加

---
+++ components/atoms/button/SecondaryButton.jsx
@@ -0,0 +1,8 @@
+export const SecondaryButton = ({ children }) => {
+  return (
+    <button class="text-white  w-14  text-base bg-orange-500 rounded-r-lg hover:opacity-80 focus:outline-none shadow-2xl  ">
+      {children}
+    </button>
+  );
+};
+
