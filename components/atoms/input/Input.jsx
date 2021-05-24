22029f37fe6bf177fc2e629f637e7f06c1ba2dc6
wataru <wata124coron@icloud.com>
2021-05-24 21:28:38 +0900

    🚀 スタイルの変更

--- components/atoms/input/Input.jsx
+++ components/atoms/input/Input.jsx
@@ -4,7 +4,7 @@ export const Input = ({ placeholder }) => {
       <input
         type="text"
         placeholder={placeholder}
-        class=" w-full h-10 focus:outline-none 	border-solid border-2 border-oreng-500 text-xs rounded-l-lg appearance-none rounded-none"
+        class=" w-full h-10 focus:outline-none 	border-solid border-2 border-oreng-500 text-sm rounded-l-lg appearance-none rounded-none"
       ></input>
     </div>
   );
