b3a81fbf87976740cbacf292f811c1703c3d7d20
wataru <wata124coron@icloud.com>
2021-05-24 21:24:32 +0900

    💄 textに変更

--- components/atoms/input/Input.jsx
+++ components/atoms/input/Input.jsx
@@ -2,7 +2,7 @@ export const Input = ({ placeholder }) => {
   return (
     <div class="w-full">
       <input
-        type="input"
+        type="text"
         placeholder={placeholder}
         class=" w-full h-10 focus:outline-none 	border-solid border-2 border-oreng-500 text-xs rounded-l-lg appearance-none rounded-none"
       ></input>
