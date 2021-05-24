4015c6eb2029cc2288f08e51b75a20ef0d6ce0ee
wataru <wata124coron@icloud.com>
2021-05-24 19:44:09 +0900

    💄 ボタンの角丸変更

--- components/atoms/input/Input.jsx
+++ components/atoms/input/Input.jsx
@@ -1,10 +1,10 @@
 export const Input = ({ placeholder }) => {
   return (
-    <div class="w-full">
+    <div class="w-full  appearance-none rounded-none">
       <input
-        type="text"
+        type="search"
         placeholder={placeholder}
-        class="w-full h-10   focus:outline-none 	border-solid border-2 border-oreng-500 text-xs rounded-none"
+        class="w-full h-10   rounded-l-lg focus:outline-none 	border-solid border-2 border-oreng-500 text-xs appearance-none rounded-none"
       ></input>
     </div>
   );
