f19d8216ba4da8c29a5cf99847b32f8c4ecc7ce3
wataru <wata124coron@icloud.com>
2021-05-24 19:55:30 +0900

    💄 3 角丸の変更

--- components/atoms/input/Input.jsx
+++ components/atoms/input/Input.jsx
@@ -1,10 +1,10 @@
 export const Input = ({ placeholder }) => {
   return (
-    <div class="w-full  appearance-none rounded-none">
+    <div class="w-full  appearance-none rounded-r-none ">
       <input
         type="search"
         placeholder={placeholder}
-        class="w-full h-10 appearance-none rounded-none  rounded-l-lg focus:outline-none 	border-solid border-2 border-oreng-500 text-xs "
+        class="w-full h-10 appearance-none rounded-r-none  rounded-l-lg focus:outline-none 	border-solid border-2 border-oreng-500 text-xs "
       ></input>
     </div>
   );
--- components/molecules/button/SearchInput.jsx
+++ components/molecules/button/SearchInput.jsx
@@ -6,10 +6,10 @@ import { Input } from "../../atoms/input/Input";
 export const SearchInput = () => {
   const iconStyle = { padding: 2, fontSize: 10 };
   return (
-    <div class="flex w-full mr-6 appearance-none rounded-none">
+    <div class="flex w-full mr-6 appearance-none rounded-r-none ">
       <Input
         placeholder={"都道府県を入力してください"}
-        class="appearance-none rounded-none"
+        class="appearance-none rounded-r-none "
       />
       <SecondaryButton>
         <SearchIcon class="w-14 h-5 text-white text-base" />
