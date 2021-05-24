9df806f7c95f08074ecf4880e1d06d8dce93bd00
wataru <wata124coron@icloud.com>
2021-05-24 20:08:17 +0900

    💄 4 角丸の変更

--- components/atoms/input/Input.jsx
+++ components/atoms/input/Input.jsx
@@ -1,10 +1,11 @@
 export const Input = ({ placeholder }) => {
   return (
-    <div class="w-full  appearance-none rounded-r-none ">
+    <div class="w-full  ">
+      {/* rounded-r-none rounded-l-lg */}
       <input
         type="search"
         placeholder={placeholder}
-        class="w-full h-10 appearance-none rounded-r-none  rounded-l-lg focus:outline-none 	border-solid border-2 border-oreng-500 text-xs "
+        class="w-full h-10 appearance-none rounded-none focus:outline-none 	border-solid border-2 border-oreng-500 text-xs "
       ></input>
     </div>
   );
--- components/molecules/button/SearchInput.jsx
+++ components/molecules/button/SearchInput.jsx
@@ -1,4 +1,4 @@
-import {SearchIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/solid";
 
 import { SecondaryButton } from "../../atoms/button/SecondaryButton";
 import { Input } from "../../atoms/input/Input";
@@ -6,11 +6,8 @@ import { Input } from "../../atoms/input/Input";
 export const SearchInput = () => {
   const iconStyle = { padding: 2, fontSize: 10 };
   return (
-    <div class="flex w-full mr-6 appearance-none rounded-r-none ">
-      <Input
-        placeholder={"都道府県を入力してください"}
-        class="appearance-none rounded-r-none "
-      />
+    <div class="flex w-full mr-6">
+      <Input placeholder={"都道府県を入力してください"} />
       <SecondaryButton>
         <SearchIcon class="w-14 h-5 text-white text-base" />
       </SecondaryButton>
