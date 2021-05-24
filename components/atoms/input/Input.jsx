286467699cb8c34e58f06aa5d69667b17d13be23
wataru <wata124coron@icloud.com>
2021-05-24 21:39:53 +0900

    ✨ 拡大を抑える

--- components/atoms/input/Input.jsx
+++ components/atoms/input/Input.jsx
@@ -2,9 +2,9 @@ export const Input = ({ placeholder }) => {
   return (
     <div class="w-full">
       <input
-        type="text"
+        type="search"
         placeholder={placeholder}
-        class=" w-full h-10 focus:outline-none 	border-solid border-2 border-oreng-500 text-sm rounded-l-lg appearance-none rounded-none"
+        class=" w-full h-10 focus:outline-none 	border-solid border-2 border-oreng-500 te rounded-l-lg appearance-none rounded-none text-sm  text-xs::placeholder"
       ></input>
     </div>
   );
