1d13cee7d2102e8df555c41c2c11c4f5237c56d3
wataru <wata124coron@icloud.com>
2021-05-25 23:36:52 +0900

    💄 スタイルの変更

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,19 +15,19 @@ export default function Home() {
       </Head>
 
       <div className="p-5">
-        <div className="flex">
+        <div className="flex mb-6">
           <SearchInput />
           <PrimaryButton>
             <LocationMarkerIcon className="w-14 h-5  text-white  " />
           </PrimaryButton>
         </div>
-        <br />
+
         <div>
           <h2 className="w-full h-8 mb-6 pl-2 bg-gray-350 text-white leading-8 font-bold">
             カフェ情報
           </h2>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
             <UserCord />
             <UserCord />
             <UserCord />
