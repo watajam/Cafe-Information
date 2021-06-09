16a3d581d5dc4534c958a5f5793c1c70e516f998
wataru <wata124coron@icloud.com>
2021-06-09 19:47:43 +0900

    💄 カードデザインの変更

--- components/atoms/cord/Cord.jsx
+++ components/atoms/cord/Cord.jsx
@@ -1,3 +1,7 @@
 export const Cord = ({ children }) => {
-  return <div className="bg-white shadow-2xl rounded-lg ">{children}</div>;
+  return (
+    <div className="bg-white shadow-2xl rounded-lg border-2 border-gray-350 ">
+      {children}
+    </div>
+  );
 }
