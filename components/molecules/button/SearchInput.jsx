a504fdb694731536ebb2b6316a28118e757bd074
wataru <wata124coron@icloud.com>
2021-06-13 19:28:07 +0900

    ✨ 検索のページ遷移

--- components/molecules/button/SearchInput.jsx
+++ components/molecules/button/SearchInput.jsx
@@ -1,11 +1,13 @@
 import { useCallback, useState } from "react";
 import { SearchIcon } from "@heroicons/react/solid";
+import { useRouter} from "next/router"
 
 import { SecondaryButton } from "../../atoms/button/SecondaryButton";
 import { Input } from "../../atoms/input/Input";
 
 export const SearchInput = ({ onSearchSubmit }) => {
   const [text, setText] = useState("");
+  const router = useRouter()
 
   const submitFrom = (e) => {
     e.preventDefault();
@@ -14,7 +16,7 @@ export const SearchInput = ({ onSearchSubmit }) => {
       return;
     }
     onSearchSubmit(encodeURIComponent(text));
-    console.log(encodeURIComponent(text));
+    router.push("/search");
   };
 
   const onChange = useCallback((e) => {
@@ -26,6 +28,7 @@ export const SearchInput = ({ onSearchSubmit }) => {
       <SecondaryButton>
         <SearchIcon className="w-14 h-5 text-white text-base" />
       </SecondaryButton>
+
       <Input
         placeholder="店名・エリア・キーワード"
         onChange={onChange}
--- pages/search.jsx
+++ pages/search.jsx
@@ -16,12 +16,12 @@ const fetcher = (url) =>
     })
     .then((res) => res.data);
 
-export default function Search() {
+export default function Search(props) {
   const [keyword, setKeyword] = useState("");
 
   const { data, error } = useSWR(
-    `/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&keyword=${keyword}&range=5&genre=G012&count=20&format=jsop`,
-    fetcher,
+    `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&keyword=${keyword}&range=5&genre=G014&count=10&format=jsonp`,
+    fetcher
   );
 
   const onSearchSubmit = useCallback(
@@ -47,6 +47,7 @@ export default function Search() {
       <div className="p-5">
         <div className="flex mb-6">
           <SearchInput onSearchSubmit={onSearchSubmit} />
+
           <div className="ml-6">
             <PrimaryButton>
               <LocationMarkerIcon className="w-14 h-5  text-white " />
@@ -56,7 +57,7 @@ export default function Search() {
 
         <div>
           <h2 className="w-full h-8 mb-6 pl-2 bg-gray-350 text-white leading-8 font-bold">
-            ?????情報
+            検索結果
           </h2>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
@@ -69,4 +70,3 @@ export default function Search() {
     </>
   );
 }
-
